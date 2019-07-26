9a44379257a6743bec1a586a11c23d98c186c19d
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-07-26 11:34:29 -0500

    First commit

---
+++ Fantastical/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fantastical.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+
+        }
+
+        [Required]
+        public string username { get; set; }
+
+        [NotMapped]
+        public bool isAdmin { get; set; }
+    }
+}
